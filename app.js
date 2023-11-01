25fd1e2195e80755f7c3e0547124ad5636c5b2a3
mnunezsa <mnunezsa95@gmail.com>
2023-11-01 12:55:21 -0400

    connect mongoose, helmet, limiter

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const { limiter } = require("./middlewares/rateLimiter");
 
 const app = express(); // connect express to the app
+mongoose.connect("mongodb://127.0.0.1:27017/newsapp");
 const { PORT = 3001 } = process.env;
+
+app.use(helmet());
+app.use(limiter);
+
 app.listen(PORT, () => console.log(`App is listening at port ${PORT}`));
