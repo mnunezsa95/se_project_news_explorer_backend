707fbb2bf8a4a58dfa74dae81c3493c69627551b
mnunezsa <mnunezsa95@gmail.com>
2023-11-01 08:43:58 -0400

    fix console.log statement

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require("express");
 
 const app = express(); // connect express to the app
 const { PORT = 3001 } = process.env;
-app.listen(PORT, () => console.log(`App is listening at port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`App is listening at port ${PORT}`);
+});
